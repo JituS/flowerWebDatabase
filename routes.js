31c5d84b031c1d436b260a7bbad3284ed5c7519f
jitendrs <jitendrs@INjitendrs.local>
2016-01-19 14:09:19 +0530

    changed server.js code

--- routes.js
+++ routes.js
@@ -4,6 +4,9 @@ var comments = require('./comments.js');
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 app.post('/comment',function(req,res,next){
 	var data= '';
 	req.on('data',function(chunk){
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var http = require('http');
 var requestHandler = require('./routes.js');
-var server = http.createServer(requestHandler);
-server.listen(process.env.OPENSHIFT_NODEJS_PORT);
+// var server = http.createServer(requestHandler);
+// server.listen(process.env.OPENSHIFT_NODEJS_PORT);
+
+var express = require('express');
+var app = express();
+var http = require('http');
+
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+// app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+http.createServer(requestHandler).listen(requestHandler.get('port') ,requestHandler.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    // server();
+});
 
